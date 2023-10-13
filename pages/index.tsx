ea20dcf0cc42e7dc92560e6d99571e5740e49251
Milinda Prasan De Silva <milindapds@hotmail.com>
2023-10-13 16:34:33 +0100

    placeholder fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,19 +32,19 @@ const HomePage: NextPage<any> = ({ home }) => {
         <Features />
         <Container>
           <div className="flex flex-col mx-auto justify-center items-center w-full lg:pt-40 pt-20 text-center space-y-4">
-            <p className="lg:text-5xl md:text-4xl text-3xl font-medium">
+            <p className="lg:text-5xl md:text-4xl text-3xl font-medium dark:text-white">
               The Website is still being built out
             </p>
-            <p className="lg:text-4xl md:text-3xl text-2xl font-medium">
+            <p className="lg:text-4xl md:text-3xl text-2xl font-medium dark:text-white">
               There's alot of ideas and things need to be done
             </p>
-            <p className="lg:text-3xl md:text-2xl text-xl font-medium">
+            <p className="lg:text-3xl md:text-2xl text-xl font-medium dark:text-white">
               Like code-refactor, being fully prismic based, SEO..etc
             </p>
-            <p className="lg:text-2xl md:text-xl text-lg font-medium">
+            <p className="lg:text-2xl md:text-xl text-lg font-medium dark:text-white">
               I'll get there, I have soo many ideas
             </p>
-            <p className="lg:text-xl md:text-lg text-md font-medium">
+            <p className="lg:text-xl md:text-lg text-md font-medium dark:text-white">
               including connecting to cool api's etc
             </p>
           </div>
